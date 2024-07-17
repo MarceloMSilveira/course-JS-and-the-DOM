f416f27d14feb09d7bdde0c4ca5f9f489e10b706
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-07-17 16:39:37 -0300

    perfect work

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
 const allCoursesContainer = document.querySelector('#individual-nanodegree-programs');
 
+function createCardWithButton() {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card--nanodegree__favorites';
+
+    const button = document.createElement('button');
+    button.className = 'button--primary';
+    button.textContent = 'Add to favorites';
+
+    cardDiv.appendChild(button);
+
+    return cardDiv;
+}
+
+function createCardWithStars() {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card--nanodegree__3stars';
+
+    const starsDiv = document.createElement('div');
+    starsDiv.className = 'three-stars  hide-stars';
+    starsDiv.textContent = '🌟 🌟 🌟';
+
+    cardDiv.appendChild(starsDiv);
+
+    return cardDiv;
+}
+
+//const document.body.appendChild(cardElement);
+
+const cardContainers = allCoursesContainer.querySelectorAll('.card__container');
+//console.log(cardContainers[1].querySelector('.card--nanodegree__title')); 
+
+cardContainers.forEach(function(value) {
+    const cardTitle = value.querySelector('.card--nanodegree__title');
+    const cardFavoriteBtn = createCardWithButton();
+    cardTitle.insertAdjacentElement('afterend',cardFavoriteBtn);
+    const cardWithStars = createCardWithStars();
+    cardFavoriteBtn.insertAdjacentElement('afterend',cardWithStars);
+})
+
 function changeText(btn) {
     const btnText = btn.textContent;
     if(btnText==='Add to favorites'){
